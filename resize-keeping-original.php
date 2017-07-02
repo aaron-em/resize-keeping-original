e479cb1df1a779324bf2d692249d1c373794fc84
Aaron Miller <me@aaron-miller.me>
2017-07-02 09:00:49 -0400

    And the evening and the morning were the first day
    (Options registration and no-op upload filter.)

--- resize-keeping-original.php
+++ resize-keeping-original.php
@@ -10,3 +10,21 @@ Author URI: https://example.com/lol
 it's a thing lol
 */
 
+
+$PLUGIN_VERSION = '0.0.1';
+$OPTION_DEFAULTS = [
+    'version' => $PLUGIN_VERSION,
+    'max_dimension' => 2048
+];
+
+if (get_option('rko_resize_version') !== $PLUGIN_VERSION) {
+    foreach ($OPTION_DEFAULTS as $key => $value) {
+        add_option('rko_' . $key, $value);
+    };
+};
+
+add_action('wp_handle_upload', 'rko_upload_resize');
+
+function rko_upload_resize($image_data) {
+    return $image_data;
+};
