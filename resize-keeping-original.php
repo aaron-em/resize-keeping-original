0bdfe9bcc46d90b441ecad965a90ddbaa6ac3572
Aaron Miller <me@aaron-miller.me>
2017-07-02 08:41:41 -0400

    Let there be a firmament in the midst of the waters

---
+++ resize-keeping-original.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Resize Keeping Original
+Plugin URI: https://example.com/lol
+Description: lol front matter to come
+Author: Aaron Miller <me@aaron-miller.me>
+Version: 0.0.1
+Author URI: https://example.com/lol
+
+it's a thing lol
+*/
+
